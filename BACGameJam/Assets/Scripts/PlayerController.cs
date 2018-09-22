55eaa6ba99c95d4e5be8cbdd4efb3c9bb9eb0a17
Christopher Matthews <chriscodymatthews@gmail.com>
2018-09-22 01:03:54 -0500

    blah

--- BACGameJam/Assets/Scripts/PlayerController.cs
+++ BACGameJam/Assets/Scripts/PlayerController.cs
@@ -9,11 +9,37 @@ public class PlayerController : MonoBehaviour {
     public float initHealth;
     float _health;
 
+    public float speed = 3f;
+    public float vertSpeed = 5f;
+
+    public GameObject groundPlane;
+
     private void Awake()
     {
         _health = initHealth;
         if (instance == null) instance = this;
 
         DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += (groundPlane.transform.forward * vertSpeed) *Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += (-groundPlane.transform.forward * vertSpeed) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += (groundPlane.transform.right * speed) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += (-groundPlane.transform.right * speed) * Time.deltaTime;
+        }
+
     }
 }
