d4a029a649655e75091bf089b8a623243687b049
unknown <smasson1998@gmail.com>
2018-09-22 00:18:24 -0500

    added gear icon, and pause menu

---
+++ BACGameJam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+	public static GameManager register;
+
+	public GameObject pauseMenuScreen;
+
+	public Text waveText;
+
+	bool gameInPlay;
+
+
+	void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+		pauseMenuScreen.SetActive(false);
+    }
+
+	void Update() {
+		
+	}
+}
