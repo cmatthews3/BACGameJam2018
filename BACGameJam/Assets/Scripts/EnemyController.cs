5fef563c1d4f3cf4288e308df6c7ab92dd63d64b
Joshua Gates <JoshuaTGates@gmail.com>
2018-09-22 17:11:00 -0500

    Worked on enemy controller

---
+++ BACGameJam/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    GameObject player;
+    public float vertSpeed = 3;
+    public float speed = 3;
+    public GameObject groundPlane;
+
+	void Start () {
+        player = PlayerController.instance.gameObject;
+	}
+	
+	void Update () {
+        if (Vector3.Distance(player.transform.position, transform.position) < .3f)
+        {
+            //enemy attack
+        }
+        else if (player.transform.position.z > transform.position.z)
+        {
+            transform.position += (groundPlane.transform.forward * vertSpeed) * Time.deltaTime;
+        }
+        else if (player.transform.position.z < transform.position.z)
+        {
+            transform.position += (-groundPlane.transform.forward * vertSpeed) * Time.deltaTime;
+        }
+        else if (player.transform.position.x > transform.position.x)
+        {
+            transform.position += (groundPlane.transform.right * speed) * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += (-groundPlane.transform.right * speed) * Time.deltaTime;
+        }
+
+	}
+}
